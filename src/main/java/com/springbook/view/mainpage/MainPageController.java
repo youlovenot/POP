9ff32ffa0577cf944ab2dc65d80d59580992484f
word611 <dotjszbxm@naver.com>
2022-02-14 18:17:37 +0900

    no message

--- src/main/java/com/springbook/view/mainpage/MainPageController.java
+++ src/main/java/com/springbook/view/mainpage/MainPageController.java
@@ -1,5 +1,8 @@
 package com.springbook.view.mainpage;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
